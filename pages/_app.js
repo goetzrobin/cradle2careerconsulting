ec76bc0f135fc5b48dc3ab0f17dc2b5a2dca68b9
Robin Goetz <tug29225@temple.edu>
2020-08-19 16:10:28 -0400

    08/19 trying to fix flickering

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.scss'
 import 'keen-slider/keen-slider.min.css';
+import {useEffect} from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && "parentElement" in jssStyles) {
+      (jssStyles.parentElement).removeChild(jssStyles);
+    }
+  });
   return <Component {...pageProps} />
 }
 
